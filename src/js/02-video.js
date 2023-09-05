143e8313adf28762ed00552874273706bef54d43
guemplen <guemplen@gmail.com>
2023-09-05 17:02:02 +0200

    Add task2 & update node pkg

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(event => {
+    const currentTime = event.seconds;
+    //console.log(currentTime);
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }),
+  1000
+);
+
+const saveTime = localStorage.getItem('videoplayer-current-time');
+
+if (saveTime !== null) {
+  player.setCurrentTime(saveTime);
+}
